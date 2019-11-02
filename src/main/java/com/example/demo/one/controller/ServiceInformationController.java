7abff18a64c49de5c86d6c143d9b95d2be6055e1
banana jelly <bananajellyez@gmail.com>
2019-11-02 20:19:18 +0400

    Add swagger feature to project

--- src/main/java/com/example/demo/one/controller/ServiceInformationController.java
+++ src/main/java/com/example/demo/one/controller/ServiceInformationController.java
@@ -1,24 +1,21 @@
 package com.example.demo.one.controller;
 
-import com.example.demo.one.dto.Item;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-import javax.validation.Valid;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/items")
-@Api("Items API")
-public class ItemController {
+@Path("service-info")
+@Api("Service Information API")
+public class ServiceInformationController {
 
-    @POST
-    @ApiOperation("Save item to wishlist")
-    public Response saveItem(@Valid Item item) {
-        return Response.ok(item)
+    @GET
+    @ApiOperation("Check application status")
+    public Response info() {
+        return Response.ok("It works!")
                 .type(MediaType.APPLICATION_JSON)
                 .encoding("UTF-8")
                 .build();
